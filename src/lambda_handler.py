5ab9f9bb42884397e7f38514d5f9bb1bd81bbf56
Rick Vega <dev.ricardo.vega@gmail.com>
2023-10-09 00:39:20 -0600

    updated naming

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import statistics
+import sys
 from datetime import datetime
 
 import boto3
@@ -17,18 +18,22 @@ engine = create_engine(os.getenv("TRANSACTIONS_DB"))
 
 
 def lambda_handler(event, context):
-    print(f"event received: {event}")
+    logger.info(f"event received: {event}")
     bucket, key = get_s3_bucket_key(event)
 
     return transaction_processor(bucket, key)
 
 
 def get_s3_bucket_key(event: dict):
-    # TODO: validate that the keys exists
-    bucket = event["Records"][0]["s3"]["bucket"]["name"]
-    key = event["Records"][0]["s3"]["object"]["key"]
-
-    return bucket, key
+    try:
+        bucket = event["Records"][0]["s3"]["bucket"]["name"]
+        key = event["Records"][0]["s3"]["object"]["key"]
+        
+        return bucket, key
+    except Exception as e:
+        logger.fatal("unable to get bucket and key because error: {error}")
+        
+        return None, None
 
 
 def get_total_balance(data: list):
@@ -61,20 +66,14 @@ def get_monthly_transactions(data: list):
     monthly_transactions = {}
     grouped_dates = {}
 
-    # Iterate through the date strings and group them by year and month
     for record in data:
-        # Parse the date string into a datetime object
         date = datetime.strptime(record[1], "%Y-%m-%d %H:%M:%S")
-
-        # Extract the year and month from the datetime object
         year = date.year
         month = date.month
 
-        # Create a nested dictionary structure if it doesn't exist
         if year not in grouped_dates:
             grouped_dates[year] = {}
 
-        # Add the month to the subdictionary if it doesn't exist
         if month not in grouped_dates[year]:
             grouped_dates[year][month] = []
 
@@ -125,20 +124,20 @@ def get_account_report(data: list, account_number: str):
     return account_report
 
 
-def send_message(sqs, message, tries=1, max_tries=3):
+def send_message(sqs, message, tries=1, max_retries=3):
     try:
         response = sqs.send_message(
             QueueUrl=os.getenv("EMAIL_NOTIFICATIONS_QUEUE_URL"), MessageBody=message
         )
         return response["MessageId"]
     except Exception as e:
-        if tries <= max_tries:
-            print(f"sending message failed at try: {tries} with error {e}")
+        if tries <= max_retries:
+            logger.info(f"sending message failed at try: {tries} with error {e}")
             tries += 1
-            print(f"retry #{tries}…")
-            return send_message(sqs, message, tries, max_tries)
+            logger.info(f"retry #{tries}…")
+            return send_message(sqs, message, tries, max_retries)
         else:
-            print("message was not send. Max retries of {max_retries} reached.")
+            logger.info(f"message was not send. Max retries of {max_retries} reached.")
             return None
 
 
@@ -193,14 +192,14 @@ def persist_to_db(account_number: str, transactions: list, account_report: dict)
             try:
                 connection.execute(transactions_query, transactions_to_insert)
                 connection.execute(report_query, report_to_insert)
-                print("transactions and report inserted successfully.")
+                logger.info("transactions and report inserted successfully.")
                 return True
             except Exception as e:
-                print(e)
+                logger.info(e)
                 connection.rollback()  # Rollback the transaction
             except IntegrityError as e:
                 # Handle any integrity constraint violations here
-                print(f"Integrity error: {e}")
+                logger.info(f"Integrity error: {e}")
                 connection.rollback()  # Rollback the transaction
                 return False
     except Exception as e:
@@ -215,74 +214,38 @@ def transaction_processor(bucket: str, key: str):
         s3 = boto3.client("s3")
         response = s3.get_object(Bucket=bucket, Key=key)
         file = response["Body"].read()
-        print("read file from s3 successfully")
+        logger.info("read file from s3 successfully")
     except Exception as e:
         logger.error(f"error: unable to download the file from s3 - {e}")
+        sys.exit(-1)
 
     csv_reader = csv.reader(file.decode("utf-8").splitlines(), delimiter=",")
     first_row = next(csv_reader)
 
     if looks_like_headers(first_row):
-        # Skip the actual header row and read the data rows
         data_rows = [row for row in csv_reader]
     else:
-        # If it doesn't look like headers, reset the cursor and treat the first row as data
         file.seek(0)
-        # Read the data rows, including the first row
         data_rows = [first_row] + [row for row in csv_reader]
 
     account_number = get_account_number(key)
 
     if not account_number:
         logger.error(f"unable to determine the account number for file: {bucket}/{key}")
+        sys.exit(-1)
 
     account_report = get_account_report(data_rows, account_number)
 
     persisted_data = persist_to_db(account_number, data_rows, account_report)
 
     if not persisted_data:
-        print("error saving to database")
+        logger.info("error saving to database")
+        sys.exit(-1)
 
     message_body = json.dumps(account_report)
 
     sqs = boto3.client("sqs")
     message_id = send_message(sqs, message_body)
+    
     if message_id:
-        print(f"message {message_id} sent successfully")
-
-
-if __name__ == "__main__":
-    event = {
-        "Records": [
-            {
-                "eventVersion": "2.0",
-                "eventSource": "aws:s3",
-                "awsRegion": "us-east-1",
-                "eventTime": "1970-01-01T00:00:00.000Z",
-                "eventName": "ObjectCreated:Put",
-                "userIdentity": {"principalId": "EXAMPLE"},
-                "requestParameters": {"sourceIPAddress": "127.0.0.1"},
-                "responseElements": {
-                    "x-amz-request-id": "EXAMPLE123456789",
-                    "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstwxyzABCDEFGH",
-                },
-                "s3": {
-                    "s3SchemaVersion": "1.0",
-                    "configurationId": "testConfigRule",
-                    "bucket": {
-                        "name": "raw-csv-public-bucket",
-                        "ownerIdentity": {"principalId": "EXAMPLE"},
-                        "arn": "arn:aws:s3:::example-bucket",
-                    },
-                    "object": {
-                        "key": "424248018_transactions_report.csv",
-                        "size": 1024,
-                        "eTag": "0123456789abcdef0123456789abcdef",
-                        "sequencer": "0A1B2C3D4E5F678901",
-                    },
-                },
-            }
-        ]
-    }
-
-    lambda_handler(event, {})
+        logger.info(f"message {message_id} sent successfully")
